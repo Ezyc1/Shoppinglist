3881c3022fc609ae2acf983496567ce16e125f78
Leonard.Cristescu <60926@gymnasium.ax>
2024-10-30 08:04:25 +0200

    Debug False

--- ShoppingList/ShoppingList/settings.py
+++ ShoppingList/ShoppingList/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-q25-7sh37d7yhlwo$u-@+1f0#iyw!*tws1tmgyi=qzqem7r7%i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
